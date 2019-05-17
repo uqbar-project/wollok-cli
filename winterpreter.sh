ab75dfff49152f7083c9c70ed16e39259732000c
Fernando Dodino <fernando.dodino@gmail.com>
2019-05-17 10:44:31 -0300

    wtf with result file?

--- winterpreter.sh
+++ winterpreter.sh
@@ -24,6 +24,7 @@ done
 DIR=`dirname "$PRG"`
 
 java -cp "$(echo `find jars/ -name "*.jar"` | sed 's# #:#g')" org.uqbar.project.wollok.launch.WollokLauncher $@ > result
+chmod 666 result
 if [ -s result ] ; then 
   grep -q "ERROR\|Exception" result
   if [ $? -eq 0 ]  ; then
