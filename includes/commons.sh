57fa619ad717abfd502cf37bfcccef0fd1f3cf5a
Fernando Dodino <fernando.dodino@gmail.com>
2019-05-24 08:45:46 -0300

    echoing for Travis build

--- includes/commons.sh
+++ includes/commons.sh
@@ -1,4 +1,6 @@
 function interpret() {
+    echo $CLI_DIR
+    echo `find $SRC -name $1 ! -path "./.history*" ! -path "./bin/**" ! -path "$CLI_DIR/examples/*"`
     for i in `find $SRC -name $1 ! -path "./.history*" ! -path "./bin/**" ! -path "$CLI_DIR/examples/*"`
         do
             echo "   ==>  $i"
