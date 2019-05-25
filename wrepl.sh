501a1970d45e58df07e69eb9f1674fe990712ab0
Fernando Dodino <fernando.dodino@gmail.com>
2019-05-25 10:31:16 -0300

    Fix #5 - help wollok bash command - enhanced README

--- wrepl.sh
+++ wrepl.sh
@@ -7,5 +7,4 @@
 CLI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $CLI_DIR/includes/prepareFile.sh
 
-#echo java -cp "$(echo $WCLASS_PATH | sed 's# #:#g')" org.uqbar.project.wollok.launch.WollokLauncher $@
 java -cp "$(echo `find $CLI_DIR/jars/ -name "*.jar"` | sed 's# #:#g')" org.uqbar.project.wollok.launch.WollokLauncher -r $@
