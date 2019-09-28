1e9d4e10ca142ca76c42bbefc6938a35940e8d22
Fernando Dodino <fernando.dodino@gmail.com>
2019-09-28 00:57:01 -0300

    Back to several files as default interpreter

--- includes/commons.sh
+++ includes/commons.sh
@@ -21,6 +21,9 @@
 
 # 
 # This should be the next version => once we fix launcher with several files
+#    arg 1:     a Wollok folder which contains all test files (.wtest)
+#    arg 2:     passing wollok-cli root folder
+#    args 3..n: any other interpreter argument
 # 
 function interpret() {
     STATUS=0
