855ac8f7ff89927e46cacf1cd3e3928aca3c9764
Fernando Dodino <fernando.dodino@gmail.com>
2019-10-06 13:23:07 -0300

    Removing previous version => running a single file at a time

--- includes/commons.sh
+++ includes/commons.sh
@@ -1,24 +1,6 @@
 #
 # Common functions
 #
-# Interprets
-#    arg 1:  a Wollok file (.wtest, .wlk, .wpgm)
-#    arg 2:  passing wollok-cli root folder
-#    ...arg: any other args
-# function interpret() {
-#     STATUS=0
-#     CLI_FOLDER=$2
-#     for file in `find -name $1 ! -path "*/.history*" ! -path "*/bin/**" ! -path "*/wsanity-check-examples/*" ! -path "*/includes/emptyFile.wlk"`
-#         do
-#             echo "   -->  $file"
-#             $CLI_FOLDER/winterpreter.sh $file exitOnBuildFailure "${@:3}"
-#             if [ $? -ne 0 ] ; then
-#                 STATUS=1
-#             fi
-#         done
-#     return $STATUS
-# }
-
 # 
 # This should be the next version => once we fix launcher with several files
 #    arg 1:     a Wollok folder which contains all test files (.wtest)
